875aa920eddf66f7c4e4c571eda4d10c61df7e59
Danuel <public.danuel@gmail.com>
2019-07-28 11:37:46 +0900

    react-hookable-store 라이브러리 추가 및 모달 컴포넌트 작성방법 변경

--- src/App/Atomics/Modal/index.tsx
+++ src/App/Atomics/Modal/index.tsx
@@ -1,6 +1,10 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, ReactNode } from 'react'
 import { createPortal } from 'react-dom'
 import styled from 'styled-components'
+import { KeyInfo } from 'key-info'
+
+import { useDispatch } from '../../Store'
+import { openModal, closeModal } from '../../Store/Modal'
 
 {
   // body 안쪽에 ModalRoot 추가
@@ -12,34 +16,38 @@ import styled from 'styled-components'
 
 declare const ModalRoot: HTMLDivElement
 
-let index = 0
-
 interface Props extends Styleable {
   readonly isOpen: boolean
-  readonly component: FC
+  readonly isClosableOverlay: boolean
+  readonly close?: () => void
+  readonly children: ReactNode
 }
 
-export const Modal: FC<Props> = ({ isOpen, className, component: Component }) => {
+export function Modal({ isOpen, isClosableOverlay, className, close = () => {}, children }: Props) {
+  const dispatch = useDispatch()
+
   const controlScroll = () => {
     if (!isOpen) {
       return
     }
 
-    const DISABLE_SCROLL = 'disable-scroll'
-
-    const FIRST = 1
-    const ZERO = 0
-
-    index += 1
-    if (index === FIRST) {
-      window.document.documentElement.classList.add(DISABLE_SCROLL)
+    const closeOnEscape = (event: KeyboardEvent) => {
+      const keyinfo = KeyInfo.from(event)
+      if (keyinfo.isEscape) {
+        close()
+      }
     }
 
+    const DISABLE_SCROLL = 'disable-scroll'
+
+    window.document.documentElement.classList.add(DISABLE_SCROLL)
+    window.addEventListener('keydown', closeOnEscape)
+    dispatch(openModal())
+
     return () => {
-      index += -1
-      if (index === ZERO) {
-        window.document.documentElement.classList.remove(DISABLE_SCROLL)
-      }
+      window.document.documentElement.classList.remove(DISABLE_SCROLL)
+      window.removeEventListener('keydown', closeOnEscape)
+      dispatch(closeModal())
     }
   }
   useEffect(controlScroll, [isOpen])
@@ -48,15 +56,21 @@ export const Modal: FC<Props> = ({ isOpen, className, component: Component }) =>
     return null
   }
 
-  const children = (
-    <Layout className={className}>
+  const onClick = () => {
+    if (isClosableOverlay) {
+      close()
+    }
+  }
+
+  const modal = (
+    <Layout className={className} onClick={onClick}>
       <div>
-        <Component />
+        <div onClick={event => event.stopPropagation()}>{children}</div>
       </div>
     </Layout>
   )
 
-  return createPortal(children, ModalRoot)
+  return createPortal(modal, ModalRoot)
 }
 
 const Layout = styled.div`
@@ -75,13 +89,100 @@ const Layout = styled.div`
 
   padding: 32px;
 
-  & > div {
-    position: relative;
+  background-color: hsl(0 0% 0% / 36%);
 
+  & > div {
     display: flex;
     justify-content: center;
     align-items: center;
 
     height: 100%;
+
+    & > div {
+      position: relative;
+      overflow: hidden;
+
+      background-color: hsl(0 0% 96%);
+      border-radius: 3px;
+      box-shadow: 0 0 3px 1px hsl(0 0% 64%);
+    }
   }
 `
+
+const TitleLayout = styled.div`
+  margin: 0 16px;
+  padding: 16px 0;
+
+  text-align: center;
+  text-transform: capitalize;
+
+  border-bottom: 1px solid hsl(0 16% 84%);
+  border-radius: 1ch;
+`
+
+interface TitleProps {
+  readonly children: string
+}
+
+const Title: FC<TitleProps> = ({ children }) => (
+  <TitleLayout>
+    <h2>{children}</h2>
+  </TitleLayout>
+)
+
+Modal.Title = Title
+
+Modal.Main = styled.div`
+  width: 100%;
+
+  margin: 0 auto;
+  padding: 32px;
+
+  p {
+    text-indent: 2ch;
+  }
+
+  p:first-child {
+    text-transform: uppercase;
+    font-weight: bolder;
+  }
+
+  p + p {
+    margin-top: 1.5ch;
+  }
+`
+
+interface ButtonProps {
+  readonly children: string
+}
+
+const Button = styled.button<ButtonProps>`
+  cursor: pointer;
+
+  width: 100%;
+
+  padding: 16px 32px;
+
+  text-align: center;
+  text-transform: capitalize;
+  font-weight: bolder;
+`
+
+Modal.Button = Button
+
+const ButtonGroupLayout = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+
+  text-align: center;
+  border-top: 1px solid hsl(0 16% 84%);
+
+  ${Button} + ${Button} {
+    border-left: 1px solid hsl(0 16% 84%);
+  }
+`
+
+const ButtonGroup: FC = ({ children }) => <ButtonGroupLayout className='button-group'>{children}</ButtonGroupLayout>
+
+Modal.ButtonGroup = ButtonGroup
